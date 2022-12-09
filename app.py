da166a917631cc8f310f5c13d0168a187c5c753a
Bhavesh <bhavesh@flash.pe>
2022-12-09 16:13:29 +0530

    commit intial codes

--- app.py
+++ app.py
@@ -10,11 +10,11 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 @app.route("/", methods=("GET", "POST"))
 def index():
     if request.method == "POST":
-        animal = request.form["animal"]
+        age = request.form["age"]
         response = openai.Completion.create(
             model="text-davinci-002",
-            prompt=generate_prompt(animal),
-            temperature=0.6,
+            prompt=generate_prompt(age),
+            temperature=0.8,
         )
         return redirect(url_for("index", result=response.choices[0].text))
 
@@ -22,14 +22,25 @@ def index():
     return render_template("index.html", result=result)
 
 
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
+def generate_prompt(age):
+    return """Juno Provide benefits that enable employees to show up as the best version of themselves: happier, healthier and more engaged.
+Employees can choose from over 500+ curated experiences that support their wellbeing. Some of the categories it provides benefits
+are Workshops, Flower Delivery, Healthy Eating, Skincare Products, Meals Kit, Fitness Apps, Spa Treatments.
+Recommend the benefits which employees in India would want based on their age  
+Age: 21
+Benefits: Udemy Credits, Audible Subscription, Netflix Subscription, Zomato Discount Codes, RedBus Discount Codes
+Age: 23
+Benefits: Udemy Credits, Ink42 Subscription, Netflix Subscription, Zomato Discount Codes, Trivago Discount Codes
+Age: 25
+Benefits: ClearTax Credits, Ink42 Subscription, Captable Subscription, Furlanco Discount Codes, BookMyShow Discount Codes, Socials Discount Coupons
+Age: 27
+Benefits: Nyka Credits, Myntra Credits, Ajio Credits, Ink42 Subscription, Captable Subscription, Furlanco Discount Codes, BookMyShow Discount Codes, Socials Discount Coupons
+Age: 30
+Benefits: Nyka Credits, UpGrad Credits, FirstCry Credits, Ink42 Subscription, UrbanCompany Subscription, Furlanco Discount Codes, BookMyShow Discount Codes
+Age:{}
+Benefits:""".format(
+        age
     )
+
+
+
